190c7aa41bcaa05c514a3963e1f6fb019b8a3156
PedroPaivaDev <111433715+PedroPaivaDev@users.noreply.github.com>
2022-11-13 21:10:11 -0300

    Quarta Versão
    Inserida a foto e adicionado alguns projetos

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// https://cdn-icons-png.flaticon.com/512/49/49408.png
+// https://www.kindpng.com/picc/m/255-2558173_github-logo-png-transparent-png.png
+
 function mudaTema() {
   document.body.classList.toggle("dark")
 }
